68a89c9a6dc0685711bdadd2098f7355469fb8dd
kaliso-dev <sosollamsouni@gmail.com>
2024-12-26 15:10:47 +0100

    first commit

--- version 1/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burgerMenu = document.querySelector('.toggle-checkbox');
-    const pages = document.querySelector('.pages');
-
-    burgerMenu.addEventListener('click', () => {
-        pages.classList.toggle('active');
-    });
-});
